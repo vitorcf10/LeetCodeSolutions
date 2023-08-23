95caa8f6fcd1d1240850976f324a5d83a481d9f9
vitorcf10 <vitor.figueira8@gmail.com>
2023-08-23 10:59:11 -0300

    Two Sum commented

--- 1_TwoSum/bruteforce.py
+++ 1_TwoSum/bruteforce.py
@@ -1,3 +1,7 @@
+'''
+This is a brute force solution for the two-sum problem. 
+It iterates through every combination of two numbers until it finds the numbers that add up to the target.
+'''
 class Solution(object):
     def twoSum(self, nums, target):
         """
@@ -5,12 +9,12 @@ class Solution(object):
         :type target: int
         :rtype: List[int]
         """
-        for i in range(len(nums)-1):
-            j=i+1
-            while (j<(len(nums))):
-                if(nums[i]+nums[j]==target):
-                    tgti=i
+        for i in range(len(nums)-1): # For loop to iterate through every element
+            j=i+1 #Set the pointer j to the next element
+            while (j<(len(nums))): # While j is less than the array length 
+                if(nums[i]+nums[j]==target): # Check if the sum of the element at index i and j add up to target
+                    tgti=i # If so save the index of i and j.
                     tgtj=j
-                j+=1
-        sol=[tgti, tgtj]
-        return sol
+                j+=1 # If the sum does not add up to target go to the next element
+        sol=[tgti, tgtj] # Solution is the combination of the index saved for i and j
+        return sol # Return solution
--- 1_TwoSum/dictionarySol.py
+++ 1_TwoSum/dictionarySol.py
@@ -1,3 +1,10 @@
+'''
+This is the dictionary/hashmap solution for the two-sum problem. 
+The main idea here is to have an auxiliary dictionary that stores every element in the array as a key and their indexes as values. 
+For every element, calculate the element that is needed for the sum to add up to the target (needed = target - element). 
+Then, check to see if the element is already in the auxiliary dictionary. 
+If it is, return the element's index and the value of the key needed in the dictionary. If it isn't, just add the element to the dictionary.
+'''
 class Solution(object):
     def twoSum(self, nums, target):
         """
@@ -5,11 +12,11 @@ class Solution(object):
         :type target: int
         :rtype: List[int]
         """
-        auxdict = dict()
-        for i in range(len(nums)):
-            need = target - nums[i]
-            if need in auxdict:
-                sol=[auxdict[need], i]
+        auxdict = dict() # Create auxiliar dictionary
+        for i in range(len(nums)): # For loop to iterate through every element in array
+            need = target - nums[i] # Calculate the needed number for current element
+            if need in auxdict: # If the needed number is in dict
+                sol=[auxdict[need], i] # Solution is the combination of the value at key=needed plus the current element index
             else:
-                auxdict[nums[i]]=i
-        return sol
+                auxdict[nums[i]]=i # If needed not in dict, add the key-value pair as number-index in the dictionary
+        return sol # Return solutions
